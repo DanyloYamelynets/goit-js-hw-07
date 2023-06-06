54fa7ccedd38d7f7210f9042e2b59a0090d2125d
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-06 23:12:52 +0300

    1 task update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const myGallery = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+
+// console.log(galleryList);
+
+myGallery.insertAdjacentHTML("beforeend", galleryList);
+myGallery.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+	<img src="${event.target.dataset.source}">
+`);
+  instance.show();
+}
